3a1132a565971f66edcb1769f2780842eb2d36cb
yorkyu <yorkyu@tencent.com>
2022-10-20 13:48:52 +0800

    feat: add functions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cloudflare Pages, Learn React
         </a>
       </header>
     </div>
